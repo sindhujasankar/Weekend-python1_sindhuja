4bbba73deda1a410726828c9dc497cf29e7c0eaf
sindhujasankar <53147954+sindhujasankar@users.noreply.github.com>
2019-07-27 13:43:27 +0530

    Add files via upload

---
+++ Python weekend class_sindhuja.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import sys
+print sys.version
+
+
+# In[2]:
+
+
+import sys
+print (sys.version)
+
+
+# In[3]:
+
+
+x=5
+y=10
+print(x+y)
+
+
+# In[5]:
+
+
+Description="Python is a interpreted language"
+print(description)
+
+
+# In[6]:
+
+
+Description="Python is a interpreted language"
+print(Description)
+
+
+# In[7]:
+
+
+first name="sindhuja"
+print(first name)
+
+
+# In[8]:
+
+
+first_name="sindhuja"
+print(first_name)
+
+
+# In[9]:
+
+
+first_name="sindhuja"
+lasy_name="sankar"
+print(first_name.title())
+
+
+# In[10]:
+
+
+name="sindhuja sankar"
+print(name.upper())
+
+
+# In[11]:
+
+
+name="SINDHUJA SANKAR"
+print(name.lower())
+
+
+# In[12]:
+
+
+first_name="sindhuja"
+last_name="sankar"
+name=f "{first_name} {last_name}"
+
+
+# In[13]:
+
+
+first_name="sindhuja"
+last_name="sankar"
+name=f "{first_name} {last_name}"
+print(name)
+
+
+# In[14]:
+
+
+first_name="sindhuja"
+last_name="sankar"
+name=f "{first_name} {last_name}""
+print(name)
+
+
+# In[17]:
+
+
+first_name='sindhuja'
+last_name='sankar'
+name=f" {first_name} {last_name} "
+print(name)
+
+
+# In[18]:
+
+
+first_name="sindhuja"
+last_name="sankar"
+name=f" {first_name} {last_name} "
+print(name)
+
+
+# In[19]:
+
+
+first_name="sindhuja"
+last_name="sankar"
+print(first_name,last_name)
+
+
+# In[1]:
+
+
+car_name="lamboghini/t/tford/tfreestyle"
+print(car_name)
+
+
+# In[2]:
+
+
+car_name="lamboghini\t\tford\tfreestyle"
+print(car_name)
+
+
+# In[3]:
+
+
+car_name="lamboghini\s\tford\tfreestyle"
+print(car_name)
+
+
+# In[6]:
+
+
+briyani_hotel="yamoidheen\nRasavi\nParadise\nApplebees"
+print(briyani_hotel)
+
+
+# In[7]:
+
+
+Cartoon_names="      Dora        bhuji      mottu    patlu"
+print(Cartoon_names.strip())
+
+
+# In[11]:
+
+
+Cartoon_names="      Dora        bhuji      mottu    patlu       "
+pet_names="dolmacian"
+print(Cartoon_names.strip(),pet_names)
+
+
+# In[9]:
+
+
+Cartoon_names="      Dora        bhuji      mottu    patlu"
+print(Cartoon_names.rstrip())
+
+
+# In[10]:
+
+
+Cartoon_names="      Dora        bhuji      mottu    patlu"
+print(Cartoon_names.lstrip())
+
+
+# In[ ]:
+
+
+
+
